1df51fb88e1360ca2c69c889c3e280794118d04d
Revivalo <revivalo@athelion.eu>
2023-03-04 17:29:43 +0100

    Add pre-check for PlaceholderAPI

--- src/main/java/dev/revivalo/dailyrewards/hooks/Hooks.java
+++ src/main/java/dev/revivalo/dailyrewards/hooks/Hooks.java
@@ -1,5 +1,6 @@
 package dev.revivalo.dailyrewards.hooks;
 
+import dev.revivalo.dailyrewards.utils.VersionUtil;
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
 
@@ -12,7 +13,7 @@ public class Hooks {
 
 
     public static void hook(){
-        PLACEHOLDER_API_HOOK = new PlaceholderApiHook();
+        if (VersionUtil.checkPlugin("PlaceholderAPI")) PLACEHOLDER_API_HOOK = new PlaceholderApiHook();
         BSTATS_HOOK = new BStatsHook();
         ORAXEN_HOOK = new OraxenHook();
         ITEMS_ADDER_HOOK = new ItemsAdderHook();

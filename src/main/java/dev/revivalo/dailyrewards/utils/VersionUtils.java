92e9337fca1155ff92c31114eecc9277e3ecaf4e
Revivalo <revivalo@athelion.eu>
2023-03-08 10:38:53 +0100

    Added VersionUtils class

---
+++ src/main/java/dev/revivalo/dailyrewards/utils/VersionUtils.java
@@ -0,0 +1,26 @@
+package dev.revivalo.dailyrewards.utils;
+
+import dev.revivalo.dailyrewards.DailyRewardsPlugin;
+import io.github.g00fy2.versioncompare.Version;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.UtilityClass;
+import org.bukkit.Bukkit;
+
+@UtilityClass
+public class VersionUtils {
+    @Getter @Setter private static boolean hexSupport;
+    @Getter @Setter private static boolean legacyVersion;
+    @Getter @Setter public static boolean latestVersion;
+
+    static {
+        final String serverVersion = Bukkit.getBukkitVersion();
+        Version version = new Version(serverVersion);
+        setHexSupport(version.isAtLeast("1.16"));
+        setLegacyVersion(version.isLowerThan("1.13"));
+    }
+
+    public static boolean checkPlugin(String pluginName){
+        return DailyRewardsPlugin.get().getPluginManager().getPlugin(pluginName) != null;
+    }
+}

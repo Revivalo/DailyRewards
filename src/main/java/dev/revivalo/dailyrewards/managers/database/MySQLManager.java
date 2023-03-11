fb9699e583a5a11580c0c32d2f4a706125bb5ba9
ChimpGamer <=>
2023-03-11 20:22:42 +0100

    Make proper use of the HikariDataSource and the connections in the pool.

--- src/main/java/dev/revivalo/dailyrewards/managers/database/MySQLManager.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/database/MySQLManager.java
@@ -25,9 +25,8 @@ public class MySQLManager {
 	private static final String INSERT = "INSERT INTO Rewards (%columns%) VALUES (%values%);";
 	private static final String UPDATE = "UPDATE Rewards SET %values% WHERE id='%id%';";
 
-	private static Connection connection;
+	private static HikariDataSource dataSource;
 
-	@SneakyThrows
 	public static void init(){
 		if (!Config.USE_MYSQL.asBoolean()) return;
 
@@ -44,10 +43,9 @@ public class MySQLManager {
 		config.setUsername(username);
 		config.setPassword(password);
 
-		HikariDataSource ds = new HikariDataSource(config);
+		dataSource = new HikariDataSource(config);
 
-		try {
-			connection = ds.getConnection();
+		try (Connection connection = getConnection()) {
 			connection.prepareStatement(CREATE_TABLE).execute();
 			DataManager.setUsingMysql(true);
 			File dir = new File(DailyRewardsPlugin.getPlugin(DailyRewardsPlugin.class).getDataFolder(), "userdata");
@@ -78,25 +76,33 @@ public class MySQLManager {
 		}
 	}
 
-	@SneakyThrows
 	public static void createPlayer(final String uuid) {
 		if (MySQLManager.playerExists(uuid)) return;
 		final long currentTimeInMillis = System.currentTimeMillis();
-		connection.prepareStatement(INSERT
-				.replace("%columns%", "id, daily, weekly, monthly")
-				.replace("%values%", uuid + ", " +
-						(Config.DAILY_AVAILABLE_AFTER_FIRST_JOIN.asBoolean() ? 0 : currentTimeInMillis + RewardType.DAILY.getCooldown()) + ", " +
-						(Config.WEEKLY_AVAILABLE_AFTER_FIRST_JOIN.asBoolean() ? 0 : currentTimeInMillis + RewardType.WEEKLY.getCooldown()) + ", " +
-						(Config.MONTHLY_AVAILABLE_AFTER_FIRST_JOIN.asBoolean() ? 0 : currentTimeInMillis + RewardType.MONTHLY.getCooldown())));
+		try (Connection connection = getConnection()) {
+			connection.prepareStatement(INSERT
+					.replace("%columns%", "id, daily, weekly, monthly")
+					.replace("%values%", uuid + ", " +
+							(Config.DAILY_AVAILABLE_AFTER_FIRST_JOIN.asBoolean() ? 0 : currentTimeInMillis + RewardType.DAILY.getCooldown()) + ", " +
+							(Config.WEEKLY_AVAILABLE_AFTER_FIRST_JOIN.asBoolean() ? 0 : currentTimeInMillis + RewardType.WEEKLY.getCooldown()) + ", " +
+							(Config.MONTHLY_AVAILABLE_AFTER_FIRST_JOIN.asBoolean() ? 0 : currentTimeInMillis + RewardType.MONTHLY.getCooldown())))
+					.execute();
+		} catch (SQLException ex) {
+			DailyRewardsPlugin.get().getLogger().warning("Failed to create player " + uuid + "!\n" + ex.getLocalizedMessage());
+		}
 	}
 
-	@SneakyThrows
 	public static boolean playerExists(final String uuid) {
-		return connection.prepareStatement(SELECT
-						.replace("%value%", "daily")
-						.replace("%selector%", uuid))
-				.executeQuery()
-				.next();
+		try (Connection connection = getConnection()) {
+			return connection.prepareStatement(SELECT
+							.replace("%value%", "daily")
+							.replace("%selector%", uuid))
+					.executeQuery()
+					.next();
+		} catch (SQLException ex) {
+			DailyRewardsPlugin.get().getLogger().warning("Failed to check if player " + uuid + " exists!\n" + ex.getLocalizedMessage());
+			return false;
+		}
 	}
 
 	public static void updateCooldown(final UUID uuid, Map<String, Object> data) throws SQLException {
@@ -113,15 +119,19 @@ public class MySQLManager {
 							? "?"
 							: "");
 		}
-		connection.prepareStatement(UPDATE
+		try (Connection connection = getConnection()) {
+			connection.prepareStatement(UPDATE
 					.replace("%values%", builder)
 					.replace("%id%", uuid.toString())
-				).executeUpdate();
+			).executeUpdate();
+		} catch (SQLException ex) {
+			DailyRewardsPlugin.get().getLogger().warning("Failed to update cooldown for " + uuid + "!\n" + ex.getLocalizedMessage());
+		}
 	}
 
 	public static long getRewardsCooldown(final UUID uuid, RewardType rewardType) {
 		long cooldown;
-		try {
+		try (Connection connection = getConnection()) {
 			final ResultSet resultSet = connection.prepareStatement(SELECT
 							.replace("%value%", rewardType.toString())
 							.replace("%selector%", uuid.toString())
@@ -129,11 +139,21 @@ public class MySQLManager {
 
 			if (!resultSet.next()) return 0;
 			cooldown = resultSet.getLong(rewardType.toString());
-			resultSet.close();
-
 		} catch (SQLException exception) {
 			return -1;
 		}
 		return cooldown;
+	}
+
+	@SneakyThrows(SQLException.class)
+	private static Connection getConnection() {
+		if (dataSource == null) {
+			throw new SQLException("Unable to get a connection from the pool because the dataSource is null");
+		}
+		Connection connection = dataSource.getConnection();
+		if (connection == null) {
+			throw new SQLException("Unable to get a connection from the pool.");
+		}
+		return connection;
 	}
 }

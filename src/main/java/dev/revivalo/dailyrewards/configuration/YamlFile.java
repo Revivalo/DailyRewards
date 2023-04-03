61cd0973debd36cb060a743be9ea7c7418f5ca4a
Revivalo <revivalo@athelion.eu>
2023-04-03 16:06:43 +0200

    Cleanup

--- src/main/java/dev/revivalo/dailyrewards/configuration/YamlFile.java
+++ src/main/java/dev/revivalo/dailyrewards/configuration/YamlFile.java
@@ -2,43 +2,81 @@ package dev.revivalo.dailyrewards.configuration;
 
 import com.tchristofferson.configupdater.ConfigUpdater;
 import dev.revivalo.dailyrewards.DailyRewardsPlugin;
-import lombok.Getter;
-import lombok.SneakyThrows;
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.yaml.snakeyaml.parser.ParserException;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 
 public class YamlFile {
 
-	@Getter
-	private final File yamlFile;
+	private final File file;
+	private final UpdateMethod updateMethod;
+	private final String filePath;
+	private YamlConfiguration configuration;
 
-	@SneakyThrows
-	public YamlFile(final String filePath, final File folder) {
-		yamlFile = new File(folder, filePath);
-		FileConfiguration configuration;
+	public YamlFile(final String filePath, final File folder, final UpdateMethod updateMethod) {
+		file = new File(folder, filePath);
+		this.updateMethod = updateMethod;
+		this.filePath = filePath;
+
+		boolean update = true;
+		switch (this.updateMethod){
+			case NEVER: update = false; break;
+			case ON_LOAD: update = !file.exists(); break;
+		}
 
 		try {
-			configuration = this.getConfiguration();
-
+			configuration = YamlConfiguration.loadConfiguration(file);
 		} catch (ParserException exception) {
-			Bukkit.getLogger().info(String.format("Format exception in %s file.", yamlFile.getName()));
+			//Bukkit.getLogger().info(String.format("Format exception in %s file.", file.getName()));
 			return;
 		}
 
-		configuration.save(yamlFile);
+		try {
+			configuration.save(file);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 		configuration.options().copyDefaults(true);
-		ConfigUpdater.update(DailyRewardsPlugin.get(),
-				filePath,
-				yamlFile,
-				Collections.emptyList());
+
+		if (update) {
+			try {
+				ConfigUpdater.update(DailyRewardsPlugin.get(), filePath, file, Collections.emptyList());
+				reload();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 	public YamlConfiguration getConfiguration() {
-		return YamlConfiguration.loadConfiguration(yamlFile);
+		return configuration;
+	}
+
+	public void reload() {
+		try {
+			configuration.load(file);
+		} catch (InvalidConfigurationException | IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public File getFile() {
+		return this.file;
+	}
+
+	public UpdateMethod getUpdateMethod() {
+		return this.updateMethod;
+	}
+
+	public String getFilePath() {
+		return this.filePath;
+	}
+
+	public enum UpdateMethod {
+		EVERYTIME, ON_LOAD, NEVER
 	}
 }

66f475248bbdbdd986fc9d41116d4324b460ea47
Revivalo <revivalo@athelion.eu>
2023-04-03 16:02:01 +0200

    Tweaked handling with Users

--- src/main/java/dev/revivalo/dailyrewards/user/UserHandler.java
+++ src/main/java/dev/revivalo/dailyrewards/user/UserHandler.java
@@ -11,13 +11,8 @@ public class UserHandler {
         return user;
     }
 
-    public static void setUsersAvailableRewards(final UUID uuid, short availableRewards){
-        Optional<User> userOptional = Optional.ofNullable(usersHashMap.getOrDefault(uuid, null));
-        userOptional.ifPresent(user -> user.setAvailableRewards(availableRewards));
-    }
-
     public static Optional<User> getUser(final UUID uuid){
-        return Optional.of(usersHashMap.get(uuid));
+        return Optional.ofNullable(usersHashMap.get(uuid));
     }
 
     public static User removeUser(final UUID uuid){

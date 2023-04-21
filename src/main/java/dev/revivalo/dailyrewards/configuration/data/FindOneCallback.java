6d7c90fb07535a8d09e416facf4039a293f48857
Revivalo <revivalo@athelion.eu>
2023-04-21 22:36:04 +0200

    String stored instead of RewardType to cover info about player settings

--- src/main/java/dev/revivalo/dailyrewards/configuration/data/FindOneCallback.java
+++ src/main/java/dev/revivalo/dailyrewards/configuration/data/FindOneCallback.java
@@ -1,9 +1,7 @@
 package dev.revivalo.dailyrewards.configuration.data;
 
-import dev.revivalo.dailyrewards.managers.reward.RewardType;
-
 import java.util.Map;
 
 public interface FindOneCallback {
-    void onQueryDone(Map<RewardType, Long> time);
+    void onQueryDone(Map<String, Long> time);
 }

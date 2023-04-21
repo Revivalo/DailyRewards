dbee79ca469957356c8e9ebbe3191f209d55192d
Revivalo <revivalo@athelion.eu>
2023-04-21 22:32:32 +0200

    String stored instead of RewardType to cover info about player settings

--- src/main/java/dev/revivalo/dailyrewards/user/User.java
+++ src/main/java/dev/revivalo/dailyrewards/user/User.java
@@ -2,31 +2,52 @@ package dev.revivalo.dailyrewards.user;
 
 import dev.revivalo.dailyrewards.managers.cooldown.Cooldown;
 import dev.revivalo.dailyrewards.managers.reward.RewardType;
-import lombok.Data;
+import lombok.Getter;
 import org.bukkit.entity.Player;
 
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-@Data
 public class User {
-    private final Player player;
-    private final Map<RewardType, Long> cooldowns;
+    @Getter private final Player player;
+    @Getter private final Map<String, Long> data;
+
+    public User(Player player, Map<String, Long> data) {
+        this.player = player;
+        this.data = data;
+    }
 
     public Cooldown getCooldownOfReward(RewardType rewardType) {
-        return new Cooldown(cooldowns.get(rewardType));
+        return new Cooldown(data.get(rewardType.toString()));
     }
 
     public Set<RewardType> getAvailableRewards(){
         Set<RewardType> availableRewards = new HashSet<>();
-        for (Map.Entry<RewardType, Long> entry : cooldowns.entrySet()) {
-            if (new Cooldown(entry.getValue()).isClaimable()) availableRewards.add(entry.getKey());
+        for (Map.Entry<String, Long> entry : data.entrySet()) {
+            if (RewardType.findByName(entry.getKey()) == null) continue;
+            if (new Cooldown(entry.getValue()).isClaimable()) availableRewards.add(RewardType.findByName(entry.getKey()));
         }
         return availableRewards;
     }
 
-    public void updateCooldowns(Map<RewardType, Long> changes) {
-        cooldowns.putAll(changes);
+    public boolean isEnabledJoinNotification() {
+        return 1 == data.get("join-notification");
+    }
+
+    public void setEnabledJoinNotification(boolean set) {
+        data.put("join-notification", set ? 1L : 0);
+    }
+
+    public boolean isEnabledAutoClaim() {
+        return 1 == data.get("auto-claim");
+    }
+
+    public void setEnabledAutoClaim(boolean set) {
+        data.put("auto-claim", set ? 1L : 0);
+    }
+
+    public void updateCooldowns(Map<String, Long> changes) {
+        data.putAll(changes);
     }
 }

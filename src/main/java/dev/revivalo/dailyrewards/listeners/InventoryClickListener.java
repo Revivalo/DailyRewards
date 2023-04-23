6d01d2bb4de9995797f4065ac852aeb66c2367c0
Revivalo <revivalo@athelion.eu>
2023-04-23 19:52:56 +0200

    Added checks for permissions

--- src/main/java/dev/revivalo/dailyrewards/listeners/InventoryClickListener.java
+++ src/main/java/dev/revivalo/dailyrewards/listeners/InventoryClickListener.java
@@ -3,6 +3,7 @@ package dev.revivalo.dailyrewards.listeners;
 import dev.revivalo.dailyrewards.DailyRewardsPlugin;
 import dev.revivalo.dailyrewards.configuration.data.DataManager;
 import dev.revivalo.dailyrewards.configuration.enums.Config;
+import dev.revivalo.dailyrewards.configuration.enums.Lang;
 import dev.revivalo.dailyrewards.managers.MenuManager;
 import dev.revivalo.dailyrewards.managers.reward.RewardType;
 import dev.revivalo.dailyrewards.user.User;
@@ -33,25 +34,42 @@ public class InventoryClickListener implements Listener {
 		} else if (slot == Config.MONTHLY_POSITION.asInt() && Config.MONTHLY_ENABLED.asBoolean()){
 			DailyRewardsPlugin.getRewardManager().claim(player, RewardType.MONTHLY, false, true);
 		} else if (slot == Config.SETTINGS_POSITION.asInt()) {
-			DailyRewardsPlugin.getMenuManager().openSettings(player, true);
+			DailyRewardsPlugin.getMenuManager().openSettings(player);
 		}
 	}
 
 	@EventHandler (ignoreCancelled = true)
 	public void inventoryClick(final InventoryClickEvent event) {
-		if (!(event.getInventory().getHolder() instanceof MenuManager.RewardSettingsInventoryHolder)) return;
-		if (event.getCurrentItem() == null) return;
+		if (!(event.getInventory().getHolder() instanceof MenuManager.RewardSettingsInventoryHolder))
+			return;
+
+		if (event.getCurrentItem() == null)
+			return;
+
 		event.setCancelled(true);
+
 		final User user = UserHandler.getUser(event.getWhoClicked().getUniqueId());
+		final Player player = user.getPlayer();
+
 		int slot = event.getSlot();
 		if (slot == Config.JOIN_NOTIFICATION_POSITION.asInt()) {
+			if (!player.hasPermission("dailyreward.settings.joinNotification")) {
+				player.sendMessage(Lang.PERMISSION_MESSAGE.asColoredString());
+				return;
+			}
+
 			user.setEnabledJoinNotification(!user.isEnabledJoinNotification());
 			DataManager.updateValues(user.getPlayer().getUniqueId(), user, new HashMap<String, Long>(){{put("join-notification", user.isEnabledJoinNotification() ? 1L : 0);}});
-			DailyRewardsPlugin.getMenuManager().openSettings(user.getPlayer(), true);
+			DailyRewardsPlugin.getMenuManager().openSettings(user.getPlayer());
 		} else if (slot == Config.AUTO_CLAIM_REWARDS_POSITION.asInt()) {
+			if (!player.hasPermission("dailyreward.settings.autoClaim")) {
+				player.sendMessage(Lang.PERMISSION_MESSAGE.asColoredString());
+				return;
+			}
+
 			user.setEnabledAutoClaim(!user.isEnabledAutoClaim());
 			DataManager.updateValues(user.getPlayer().getUniqueId(), user, new HashMap<String, Long>(){{put("auto-claim", user.isEnabledAutoClaim() ? 1L : 0);}});
-			DailyRewardsPlugin.getMenuManager().openSettings(user.getPlayer(), true);
+			DailyRewardsPlugin.getMenuManager().openSettings(user.getPlayer());
 		} else if (slot == Config.SETTINGS_POSITION.asInt()) {
 			DailyRewardsPlugin.getMenuManager().openRewardsMenu(user.getPlayer());
 		}

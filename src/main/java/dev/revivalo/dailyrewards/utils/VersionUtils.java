303394fe40ee5747e1f6690d96366c279c32aa39
Revivalo <revivalo@athelion.eu>
2023-07-06 22:04:55 +0200

    Added check for versions below 1.9

--- src/main/java/dev/revivalo/dailyrewards/utils/VersionUtils.java
+++ src/main/java/dev/revivalo/dailyrewards/utils/VersionUtils.java
@@ -10,6 +10,7 @@ import org.bukkit.Bukkit;
 @UtilityClass
 public class VersionUtils {
     @Getter @Setter private static boolean hexSupport;
+    @Getter @Setter private static boolean oldVersion;
     @Getter @Setter private static boolean legacyVersion;
     @Getter @Setter public static boolean latestVersion;
 
@@ -17,6 +18,7 @@ public class VersionUtils {
         final String serverVersion = Bukkit.getBukkitVersion();
         Version version = new Version(serverVersion);
         setHexSupport(version.isAtLeast("1.16"));
+        setOldVersion(version.isLowerThan("1.9"));
         setLegacyVersion(version.isLowerThan("1.13"));
     }
 

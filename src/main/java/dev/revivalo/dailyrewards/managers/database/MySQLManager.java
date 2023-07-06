762e5c82eabd3ac16e522f9297283363f965abf1
Revivalo <revivalo@athelion.eu>
2023-07-06 22:18:04 +0200

    Altered table for new settings columns

--- src/main/java/dev/revivalo/dailyrewards/managers/database/MySQLManager.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/database/MySQLManager.java
@@ -6,12 +6,10 @@ import dev.revivalo.dailyrewards.DailyRewardsPlugin;
 import dev.revivalo.dailyrewards.configuration.data.DataManager;
 import dev.revivalo.dailyrewards.configuration.enums.Config;
 import dev.revivalo.dailyrewards.managers.reward.RewardType;
+import dev.revivalo.dailyrewards.utils.TextUtils;
 import dev.revivalo.dailyrewards.utils.VersionUtils;
 import lombok.SneakyThrows;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.File;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -20,9 +18,10 @@ import java.util.*;
 public class MySQLManager {
 
 	private static final String CREATE_TABLE = "CREATE TABLE IF NOT EXISTS Rewards(id varchar(255), daily long, weekly long, monthly long);";
+	private static final String ADD_REWARD_COLUMN = "ALTER TABLE Rewards ADD COLUMN `%column%` int DEFAULT %default%;";
 	private static final String SELECT = "SELECT %value% FROM Rewards WHERE id='%selector%';";
 	private static final String INSERT = "INSERT INTO Rewards (%columns%) VALUES (%values%);";
-	private static final String UPDATE = "UPDATE Rewards SET %values% WHERE id='%id%';";
+	private static final String UPDATE = "UPDATE Rewards SET %values% WHERE id='%selector%';";
 
 	private static HikariDataSource dataSource;
 
@@ -52,10 +51,12 @@ public class MySQLManager {
 
 		try (Connection connection = getConnection()) {
 			connection.prepareStatement(CREATE_TABLE).execute();
-			connection.prepareStatement("ALTER TABLE Rewards ADD COLUMN `autoClaim` boolean DEFAULT " + Config.AUTO_CLAIM_REWARDS_ON_JOIN_BY_DEFAULT.asBoolean() + ";");
-			connection.prepareStatement("ALTER TABLE Rewards ADD COLUMN `joinNotification` boolean DEFAULT " + Config.JOIN_NOTIFICATION_BY_DEFAULT.asBoolean() + ";");
+			addNewRewardColumn(new HashMap<String, String>(){{put("joinNotification", Config.JOIN_NOTIFICATION_BY_DEFAULT.asBoolean() ? "1" : "0");}});
+			addNewRewardColumn(new HashMap<String, String>(){{put("autoClaim", Config.AUTO_CLAIM_REWARDS_ON_JOIN_BY_DEFAULT.asBoolean() ? "1" : "0");}});
+			//connection.prepareStatement("ALTER TABLE Rewards ADD COLUMN `autoClaim` boolean DEFAULT " + Config.AUTO_CLAIM_REWARDS_ON_JOIN_BY_DEFAULT.asBoolean() + ";").executeBatch();
+			//connection.prepareStatement("ALTER TABLE Rewards ADD COLUMN `joinNotification` boolean DEFAULT " + Config.JOIN_NOTIFICATION_BY_DEFAULT.asBoolean() + ";").executeBatch();
 			DataManager.setUsingMysql(true);
-			File dir = new File(DailyRewardsPlugin.getPlugin(DailyRewardsPlugin.class).getDataFolder(), "userdata");
+			/*File dir = new File(DailyRewardsPlugin.getPlugin(DailyRewardsPlugin.class).getDataFolder(), "userdata");
 			File[] directoryListing = dir.listFiles();
 			if (directoryListing != null) {
 				for (File file : directoryListing) {
@@ -77,14 +78,16 @@ public class MySQLManager {
 												+ "'"))
 								.executeUpdate();
 				}
-			}
+			}*/
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	public static void createPlayer(final String uuid) {
-		if (MySQLManager.playerExists(uuid)) return;
+		if (MySQLManager.playerExists(uuid))
+			return;
+
 		final long currentTimeInMillis = System.currentTimeMillis();
 		try (Connection connection = getConnection()) {
 			String statement = INSERT
@@ -113,12 +116,13 @@ public class MySQLManager {
 		}
 	}
 
-	public static void updateCooldown(final UUID uuid, Map<String, Long> data) throws SQLException {
+	public static void updatePlayer(UUID uniqueId, Map<String, Object> changes) {
+
 		final StringBuilder builder = new StringBuilder();
-		Iterator<String> keys = data.keySet().iterator();
-		while (keys.hasNext()){
+		Iterator<String> keys = changes.keySet().iterator();
+		while (keys.hasNext()) {
 			String key = keys.next();
-			long value = data.get(key);
+			Object value = changes.get(key);
 			builder.append(key)
 					.append("='")
 					.append(value)
@@ -127,19 +131,24 @@ public class MySQLManager {
 							? ","
 							: "");
 		}
+
 		try (Connection connection = getConnection()) {
+			String command = TextUtils.replaceString(UPDATE,
+					new HashMap<String, String>() {{
+						put("%values%", builder.toString());
+						put("%selector%", uniqueId.toString());
+					}});
+			//Bukkit.getLogger().info(command);
 			connection.prepareStatement(
-					UPDATE
-						.replace("%values%", builder)
-						.replace("%id%", uuid.toString())
+					command
 			).executeUpdate();
 		} catch (SQLException ex) {
-			DailyRewardsPlugin.get().getLogger().warning("Failed to update cooldown for " + uuid + "!\n" + ex.getLocalizedMessage());
+			DailyRewardsPlugin.get().getLogger().warning("Failed to update cooldown for " + uniqueId + "!\n" + ex.getLocalizedMessage());
 		}
 	}
 
-	public static Map<String, Long> getRewardsCooldown(final UUID uuid) {
-		final Map<String, Long> cooldowns = new HashMap<>();
+	public static Map<String, Object> getRewardsCooldown(final UUID uuid) {
+		final Map<String, Object> cooldowns = new HashMap<>();
 		try (Connection connection = getConnection()) {
 			final ResultSet resultSet = connection.prepareStatement(SELECT
 					.replace("%value%", "*")
@@ -147,18 +156,40 @@ public class MySQLManager {
 			).executeQuery();
 
 			while (resultSet.next()){
-				cooldowns.put(RewardType.DAILY.name(), resultSet.getLong("daily"));
-				cooldowns.put(RewardType.WEEKLY.name(), resultSet.getLong("weekly"));
-				cooldowns.put(RewardType.MONTHLY.name(), resultSet.getLong("monthly"));
+				cooldowns.put(RewardType.DAILY.toString(), resultSet.getLong("daily"));
+				cooldowns.put(RewardType.WEEKLY.toString(), resultSet.getLong("weekly"));
+				cooldowns.put(RewardType.MONTHLY.toString(), resultSet.getLong("monthly"));
 
-				cooldowns.put("auto-claim", resultSet.getLong("autoClaim"));
-				cooldowns.put("join-notification", resultSet.getLong("joinNotification"));
+				cooldowns.put("autoClaim", resultSet.getObject("autoClaim"));
+				cooldowns.put("joinNotification", resultSet.getObject("joinNotification"));
 			}
 
 		} catch (SQLException exception) {
 			return Collections.emptyMap();
 		}
+
 		return cooldowns;
+	}
+
+	public static void addNewRewardColumn(Map<String, String> keyAndValues) {
+		try (Connection connection = getConnection();
+			 java.sql.Statement statement = connection.createStatement()) {
+			for (Map.Entry<String, String> entry : keyAndValues.entrySet()) {
+				String columnName = entry.getKey();
+				String defaultValue = entry.getValue();
+				if (!connection.getMetaData().getColumns(null, null, "Rewards", columnName).next())
+					statement.addBatch(TextUtils.replaceString(ADD_REWARD_COLUMN,
+							new HashMap<String, String>() {{
+								put("%column%", columnName);
+								put("%default%", defaultValue);
+							}}
+					));
+			}
+
+			statement.executeBatch();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@SneakyThrows(SQLException.class)

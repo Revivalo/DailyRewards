9a66326f65c68193ba14d93a849db36b534eeb88
Revivalo <revivalo@athelion.eu>
2023-08-17 17:01:25 +0200

    Changed annotation

--- src/main/java/dev/revivalo/dailyrewards/utils/ItemBuilder.java
+++ src/main/java/dev/revivalo/dailyrewards/utils/ItemBuilder.java
@@ -1,17 +1,17 @@
 package dev.revivalo.dailyrewards.utils;
 
-import lombok.NonNull;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Objects;
 
 public class ItemBuilder {
-    public static ItemBuilderBuilder from(@NonNull ItemStack itemStack) {
+    public static ItemBuilderBuilder from(@NotNull ItemStack itemStack) {
         return new ItemBuilderBuilder(itemStack);
     }
 

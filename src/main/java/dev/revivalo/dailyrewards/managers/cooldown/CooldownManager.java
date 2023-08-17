1bfc27135be5712187d5c9a09ea240264e162165
Revivalo <revivalo@athelion.eu>
2023-08-17 17:07:46 +0200

    Changed argument type

--- src/main/java/dev/revivalo/dailyrewards/managers/cooldown/CooldownManager.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/cooldown/CooldownManager.java
@@ -1,17 +1,17 @@
 package dev.revivalo.dailyrewards.managers.cooldown;
 
 import dev.revivalo.dailyrewards.configuration.data.DataManager;
-import dev.revivalo.dailyrewards.managers.reward.RewardType;
+import dev.revivalo.dailyrewards.managers.reward.Reward;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 
 public class CooldownManager {
 
-	public static void setCooldown(final Player player, RewardType type) {
+	public static void setCooldown(final Player player, Reward reward) {
 		DataManager.updateValues(player.getUniqueId(),
 				null, new HashMap<String, Object>(){{
-					put(type.toString(), System.currentTimeMillis() + type.getCooldown());
+					put(reward.getRewardName(), System.currentTimeMillis() + reward.getCooldown());
 				}}
 		);
 	}

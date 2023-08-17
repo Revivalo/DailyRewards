5e208225e51d02af36d87e61f3d5aedd6e5fd3fe
Revivalo <revivalo@athelion.eu>
2023-08-17 17:04:21 +0200

    Enums were replaced by Reward object

--- src/main/java/dev/revivalo/dailyrewards/managers/reward/RewardType.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/reward/RewardType.java
@@ -1,26 +1,13 @@
 package dev.revivalo.dailyrewards.managers.reward;
 
-import dev.revivalo.dailyrewards.configuration.enums.Config;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 import java.util.Arrays;
 
-@RequiredArgsConstructor
+
 public enum RewardType {
-	DAILY(Config.DAILY_COOLDOWN.asLong(), Config.DAILY_ENABLED.asBoolean(), Config.DAILY_COOLDOWN_FORMAT.asString()),
-	WEEKLY(Config.WEEKLY_COOLDOWN.asLong(), Config.WEEKLY_ENABLED.asBoolean(), Config.WEEKLY_COOLDOWN_FORMAT.asString()),
-	MONTHLY(Config.MONTHLY_COOLDOWN.asLong(), Config.MONTHLY_ENABLED.asBoolean(), Config.MONTHLY_COOLDOWN_FORMAT.asString());
+	DAILY(),
+	WEEKLY(),
+	MONTHLY();
 
-	@Getter private final long cooldown;
-	@Getter private final boolean enabled;
-	@Getter private final String cooldownFormat;
-
-	public static RewardType findByCooldown(long cooldown) {
-		return Arrays.stream(RewardType.values())
-				.filter(value -> value.getCooldown() == cooldown).findFirst()
-				.orElse(null);
-	}
 
 	public static RewardType findByName(String name) {
 		return Arrays.stream(RewardType.values())
@@ -32,4 +19,5 @@ public enum RewardType {
 	public String toString() {
 		return this.name().toLowerCase();
 	}
+
 }

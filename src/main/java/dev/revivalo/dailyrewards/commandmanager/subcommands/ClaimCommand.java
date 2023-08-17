8a12c2a59d247a0668b0bf13663c09be4a6eef0c
Revivalo <revivalo@athelion.eu>
2023-08-17 20:22:56 +0200

    Enhanced tab-completion

--- src/main/java/dev/revivalo/dailyrewards/commandmanager/subcommands/ClaimCommand.java
+++ src/main/java/dev/revivalo/dailyrewards/commandmanager/subcommands/ClaimCommand.java
@@ -3,11 +3,11 @@ package dev.revivalo.dailyrewards.commandmanager.subcommands;
 import dev.revivalo.dailyrewards.DailyRewardsPlugin;
 import dev.revivalo.dailyrewards.commandmanager.SubCommand;
 import dev.revivalo.dailyrewards.configuration.enums.Lang;
+import dev.revivalo.dailyrewards.managers.reward.Reward;
 import dev.revivalo.dailyrewards.managers.reward.RewardType;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -35,9 +35,7 @@ public class ClaimCommand implements SubCommand {
 
     @Override
     public List<String> getTabCompletion(CommandSender sender, int index, String[] args) {
-        List<String> commands = Arrays.stream(RewardType.values()).map(reward -> reward.toString()).collect(Collectors.toList());
-        commands.remove("all");
-        return commands;
+        return DailyRewardsPlugin.getRewardManager().getRewards().stream().map(Reward::getRewardName).collect(Collectors.toList());
     }
 
     @Override

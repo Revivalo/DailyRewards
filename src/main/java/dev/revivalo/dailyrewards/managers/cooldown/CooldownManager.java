a0b202f8a970e23d994a9345c65f7ca995cd1c33
Revivalo <revivalo@athelion.eu>
2023-08-21 11:05:36 +0200

    Removed debug message

--- src/main/java/dev/revivalo/dailyrewards/managers/cooldown/CooldownManager.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/cooldown/CooldownManager.java
@@ -3,14 +3,12 @@ package dev.revivalo.dailyrewards.managers.cooldown;
 import dev.revivalo.dailyrewards.configuration.data.DataManager;
 import dev.revivalo.dailyrewards.managers.reward.Reward;
 import dev.revivalo.dailyrewards.user.User;
-import org.bukkit.Bukkit;
 
 import java.util.HashMap;
 
 public class CooldownManager {
 
 	public static void setCooldown(final User user, Reward reward) {
-		Bukkit.getLogger().info(reward.getCooldown() + "");
 		DataManager.updateValues(
 				user.getPlayer().getUniqueId(),
 				user,

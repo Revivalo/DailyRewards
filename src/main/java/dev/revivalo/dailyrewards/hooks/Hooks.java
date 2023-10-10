9dda69bc731d12df43de3d9a46f34eb17e0b43b5
Revivalo <revivalo@athelion.eu>
2023-10-10 14:40:47 +0200

    Registered hooks for auth plugins

--- src/main/java/dev/revivalo/dailyrewards/hooks/Hooks.java
+++ src/main/java/dev/revivalo/dailyrewards/hooks/Hooks.java
@@ -1,21 +1,36 @@
 package dev.revivalo.dailyrewards.hooks;
 
 public class Hooks {
+    private static boolean authUsed = false;
+
     private static PlaceholderApiHook PLACEHOLDER_API_HOOK;
     private static BStatsHook BSTATS_HOOK;
     private static OraxenHook ORAXEN_HOOK;
     private static ItemsAdderHook ITEMS_ADDER_HOOK;
-
+    private static NexAuthHook NEX_AUTH_HOOK;
+    private static AuthMeHook AUTH_ME_HOOK;
 
     public static void hook() {
         PLACEHOLDER_API_HOOK = new PlaceholderApiHook();
         BSTATS_HOOK = new BStatsHook();
         ORAXEN_HOOK = new OraxenHook();
         ITEMS_ADDER_HOOK = new ItemsAdderHook();
+        NEX_AUTH_HOOK = new NexAuthHook();
+        AUTH_ME_HOOK = new AuthMeHook();
+
+        setAuthUsed(AUTH_ME_HOOK.isOn() || NEX_AUTH_HOOK.isOn());
     }
 
     public static <T> boolean isHookEnabled(Hook<T> hook) {
         return hook != null && hook.isOn();
+    }
+
+    public static boolean isAuthUsed() {
+        return authUsed;
+    }
+
+    public static void setAuthUsed(boolean authUsed) {
+        Hooks.authUsed = authUsed;
     }
 
     public static PlaceholderApiHook getPlaceholderApiHook() {
@@ -32,5 +47,13 @@ public class Hooks {
 
     public static ItemsAdderHook getItemsAdderHook() {
         return ITEMS_ADDER_HOOK;
+    }
+
+    public static NexAuthHook getNexAuthHook() {
+        return NEX_AUTH_HOOK;
+    }
+
+    public static AuthMeHook getAuthMeHook() {
+        return AUTH_ME_HOOK;
     }
 }

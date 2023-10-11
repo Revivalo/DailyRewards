b214f6561a20c2eee19c66cbb267943f401574f4
Revivalo <revivalo@athelion.eu>
2023-10-11 22:26:51 +0200

    API events

---
+++ src/main/java/dev/revivalo/dailyrewards/api/events/PlayerClaimRewardEvent.java
@@ -0,0 +1,41 @@
+package dev.revivalo.dailyrewards.api.events;
+
+import dev.revivalo.dailyrewards.managers.reward.RewardType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerClaimRewardEvent extends Event {
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private final Player claimer;
+    private final RewardType claimedReward;
+
+    public PlayerClaimRewardEvent(Player claimer, RewardType claimedReward) {
+        this.claimer = claimer;
+        this.claimedReward = claimedReward;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    @SuppressWarnings("unused")
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+
+    @SuppressWarnings("unused")
+    public Player getClaimer() {
+        return claimer;
+    }
+
+    @SuppressWarnings("unused")
+    public RewardType getClaimedReward() {
+        return claimedReward;
+    }
+}

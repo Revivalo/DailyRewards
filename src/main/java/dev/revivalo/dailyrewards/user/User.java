d9893fe90439f62bd4ce5e7e6f4a5e221a527dbc
Revivalo <revivalo@athelion.eu>
2023-10-11 21:58:57 +0200

    Removed permission check

--- src/main/java/dev/revivalo/dailyrewards/user/User.java
+++ src/main/java/dev/revivalo/dailyrewards/user/User.java
@@ -70,10 +70,6 @@ public class User {
         data.putAll(changes);
     }
 
-    public boolean hasPermission(String permission) {
-        return player.hasPermission(permission);
-    }
-
     public Player getPlayer() {
         return player;
     }

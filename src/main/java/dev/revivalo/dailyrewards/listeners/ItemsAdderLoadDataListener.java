e7ee80d4ced9645e72df1ed75bc57d57617083c5
Revivalo <revivalo@athelion.eu>
2023-10-21 13:55:46 +0200

    Simplified

--- src/main/java/dev/revivalo/dailyrewards/listeners/ItemsAdderLoadDataListener.java
+++ src/main/java/dev/revivalo/dailyrewards/listeners/ItemsAdderLoadDataListener.java
@@ -2,18 +2,12 @@ package dev.revivalo.dailyrewards.listeners;
 
 import dev.lone.itemsadder.api.Events.ItemsAdderLoadDataEvent;
 import dev.revivalo.dailyrewards.DailyRewardsPlugin;
-import dev.revivalo.dailyrewards.configuration.YamlFile;
-import dev.revivalo.dailyrewards.configuration.enums.Config;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import java.util.Objects;
 
 public class ItemsAdderLoadDataListener implements Listener {
     @EventHandler
     public void onItemsAdderLoad(final ItemsAdderLoadDataEvent event){
-        Config.loadItems(Objects.requireNonNull(new YamlFile("config.yml",
-                DailyRewardsPlugin.get().getDataFolder(), YamlFile.UpdateMethod.EVERYTIME)
-                .getConfiguration().getConfigurationSection("config")));
+        DailyRewardsPlugin.get().reloadPlugin();
     }
 }

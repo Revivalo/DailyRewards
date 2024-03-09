5916039a54c804eca2e0223c445c7d2f999b57c3
Adam Bíba <revivalo@athelion.eu>
2024-03-09 19:27:13 +0100

    Removed unused method

--- src/main/java/dev/revivalo/dailyrewards/utils/PermissionUtils.java
+++ src/main/java/dev/revivalo/dailyrewards/utils/PermissionUtils.java
@@ -1,6 +1,5 @@
 package dev.revivalo.dailyrewards.utils;
 
-import dev.revivalo.dailyrewards.user.User;
 import org.bukkit.command.CommandSender;
 
 public class PermissionUtils {
@@ -22,10 +21,6 @@ public class PermissionUtils {
         }
     }
 
-    public static boolean hasPermission(User user, Permission permission) {
-        return hasPermission(user.getPlayer(), permission);
-    }
-
     public enum Permission {
         ADMIN_PERMISSION("dailyreward.admin"),
         RELOAD_PLUGIN("dailyreward.reload"),
@@ -34,7 +29,6 @@ public class PermissionUtils {
         JOIN_NOTIFICATION_SETTING("dailyreward.settings.joinnotification"),
         AUTO_CLAIM_SETTING("dailyreward.settings.autoclaim"),
         REQUIRED_PLAYTIME_BYPASS("dailyreward.requiredplaytime.bypass"),
-        CLAIM_REWARDS("dailyreward.claim"),
         RESET_FOR_OTHERS("dailyreward.manage"),
         HELP("dailyreward.help");
 

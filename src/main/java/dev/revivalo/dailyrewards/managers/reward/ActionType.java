7a7fd6b6850eae606d53ecc9cdcacbf781e0d822
Adam Bíba <revivalo@athelion.eu>
2024-03-09 19:26:06 +0100

    Added action types

---
+++ src/main/java/dev/revivalo/dailyrewards/managers/reward/ActionType.java
@@ -0,0 +1,13 @@
+package dev.revivalo.dailyrewards.managers.reward;
+
+public enum ActionType {
+    CONSOLE,
+    PLAYER,
+    MESSAGE,
+    TITLE,
+    SUBTITLE,
+    ACTIONBAR,
+    BOSSBAR,
+    FIREWORK,
+    SOUND
+}

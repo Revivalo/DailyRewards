17656d32de0e04427ae2879a3e7a410d0697d630
Adam Bíba <revivalo@athelion.eu>
2024-03-20 19:57:17 +0100

    Added short text descriptions for action responses

--- src/main/java/dev/revivalo/dailyrewards/managers/reward/actions/responses/ClaimActionResponse.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/reward/actions/responses/ClaimActionResponse.java
@@ -1,8 +1,22 @@
 package dev.revivalo.dailyrewards.managers.reward.actions.responses;
 
+import dev.revivalo.dailyrewards.configuration.enums.Lang;
+
 public enum ClaimActionResponse implements ActionResponse {
-    UNAVAILABLE_REWARD,
-    UNKNOWN,
-    INSUFFICIENT_PERMISSIONS,
-    NOT_ENOUGH_REQUIRED_INVENTORY_SLOTS
+    UNAVAILABLE_REWARD(Lang.UNAVAILABLE_REWARD),
+    UNKNOWN(null),
+    INSUFFICIENT_PERMISSIONS(Lang.INSUFFICIENT_PERMISSIONS),
+    INSUFFICIENT_PLAY_TIME(Lang.INSUFFICIENT_PLAY_TIME),
+    LOCATED_IN_DISABLED_WORLD(Lang.LOCATED_IN_RESTRICTED_WORLD),
+    NOT_ENOUGH_REQUIRED_INVENTORY_SLOTS(Lang.NOT_ENOUGH_FREE_INVENTORY_SLOTS);
+
+    private final Lang message;
+
+    ClaimActionResponse(Lang message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message.asColoredString();
+    }
 }

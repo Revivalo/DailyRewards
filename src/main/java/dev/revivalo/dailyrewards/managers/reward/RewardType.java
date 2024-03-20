c794c0257cccae3ddc06f3cd4244be52bbea3796
Adam Bíba <revivalo@athelion.eu>
2024-03-20 19:58:13 +0100

    Added customizable name of the reward

--- src/main/java/dev/revivalo/dailyrewards/managers/reward/RewardType.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/reward/RewardType.java
@@ -1,17 +1,21 @@
 package dev.revivalo.dailyrewards.managers.reward;
 
+import dev.revivalo.dailyrewards.configuration.enums.Lang;
+
 import java.util.Arrays;
 
 
 public enum RewardType {
-	DAILY("dailyreward.daily"),
-	WEEKLY("dailyreward.weekly"),
-	MONTHLY("dailyreward.monthly");
+	DAILY("dailyreward.daily", Lang.DAILY_NAME),
+	WEEKLY("dailyreward.weekly", Lang.WEEKLY_NAME),
+	MONTHLY("dailyreward.monthly", Lang.MONTHLY_NAME);
 
 	private final String permission;
+	private final Lang name;
 
-	RewardType(String permission) {
+	RewardType(String permission, Lang name) {
 		this.permission = permission;
+		this.name = name;
 	}
 
 	public static RewardType findByName(String name) {
@@ -25,6 +29,9 @@ public enum RewardType {
 		return this.name().toLowerCase();
 	}
 
+	public String getName() {
+		return name.asColoredString();
+	}
 
 	public String getPermission() {
 		return permission;

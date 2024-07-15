a2b45e0dd13ea5907025422dee8c97591aeb206f
Adam Bíba <revivalo@athelion.eu>
2024-07-15 23:04:41 +0200

    Fixed notification contained disabled rewards

--- src/main/java/dev/revivalo/dailyrewards/managers/reward/RewardType.java
+++ src/main/java/dev/revivalo/dailyrewards/managers/reward/RewardType.java
@@ -7,18 +7,20 @@ import java.util.Arrays;
 
 
 public enum RewardType {
-	DAILY("dailyreward.daily", Lang.DAILY_NAME, Config.DAILY_PLACEHOLDER),
-	WEEKLY("dailyreward.weekly", Lang.WEEKLY_NAME, Config.WEEKLY_PLACEHOLDER),
-	MONTHLY("dailyreward.monthly", Lang.MONTHLY_NAME, Config.MONTHLY_PLACEHOLDER);
+	DAILY("dailyreward.daily", Lang.DAILY_NAME, Config.DAILY_PLACEHOLDER, Config.DAILY_ENABLED),
+	WEEKLY("dailyreward.weekly", Lang.WEEKLY_NAME, Config.WEEKLY_PLACEHOLDER, Config.WEEKLY_ENABLED),
+	MONTHLY("dailyreward.monthly", Lang.MONTHLY_NAME, Config.MONTHLY_PLACEHOLDER, Config.MONTHLY_ENABLED);
 
 	private final String permission;
 	private final Lang name;
 	private final Config placeholder;
+	private final Config enabled;
 
-	RewardType(String permission, Lang name, Config placeholder) {
+	RewardType(String permission, Lang name, Config placeholder, Config enabled) {
 		this.permission = permission;
 		this.name = name;
 		this.placeholder = placeholder;
+		this.enabled = enabled;
 	}
 
 	public static RewardType findByName(String name) {
@@ -43,4 +45,6 @@ public enum RewardType {
 	public String getPermission() {
 		return permission;
 	}
+
+	public boolean isEnabled() {return enabled.asBoolean();}
 }

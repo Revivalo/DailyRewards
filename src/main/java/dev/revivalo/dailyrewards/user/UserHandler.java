d408cca85b694c66a11887ce1efe47a78df9c3c4
Adam Bíba <revivalo@athelion.eu>
2024-09-06 22:35:11 +0200

    User handler now handles join/quit events

--- src/main/java/dev/revivalo/dailyrewards/user/UserHandler.java
+++ src/main/java/dev/revivalo/dailyrewards/user/UserHandler.java
@@ -1,15 +1,38 @@
 package dev.revivalo.dailyrewards.user;
 
+import dev.revivalo.dailyrewards.DailyRewardsPlugin;
+import dev.revivalo.dailyrewards.api.event.ReminderReceiveEvent;
 import dev.revivalo.dailyrewards.configuration.data.DataManager;
+import dev.revivalo.dailyrewards.hook.HookManager;
+import dev.revivalo.dailyrewards.manager.Setting;
+import dev.revivalo.dailyrewards.manager.reward.RewardType;
+import dev.revivalo.dailyrewards.manager.reward.task.JoinNotificationTask;
+import dev.revivalo.dailyrewards.util.PermissionUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
-public final class UserHandler {
-    private static final HashMap<UUID, User> usersHashMap = new HashMap<>();
+public final class UserHandler implements Listener {
+    private static final Map<UUID, User> usersHashMap = new ConcurrentHashMap<>();
+
+    private final JoinNotificationTask joinNotificationTask;
+
+    public UserHandler() {
+        this.joinNotificationTask = new JoinNotificationTask(usersHashMap);
+        this.joinNotificationTask.get()
+                        .runTaskTimerAsynchronously(DailyRewardsPlugin.get(), 5, 5);
+
+        DailyRewardsPlugin.get().registerListeners(this);
+    }
+
     public static User addUser(final User user){
         usersHashMap.put(user.getPlayer().getUniqueId(), user);
         return user;
@@ -27,10 +50,62 @@ public final class UserHandler {
                 .orElse(null);
     }
 
-    public static void removeUser(final UUID uuid){
+    public static User removeUser(final UUID uuid){
         final User user = usersHashMap.remove(uuid);
 
-        if (user != null)
+        if (user != null) {
             DataManager.updateValues(uuid, user, user.getData());
+        }
+
+        return user;
+    }
+
+    @EventHandler (priority = EventPriority.HIGH)
+    private void onJoin(PlayerJoinEvent event) {
+        final Player player = event.getPlayer();
+
+        DataManager.loadPlayerDataAsync(player, data -> {
+
+            User user = UserHandler.addUser(
+                    new User(
+                            player,
+                            data
+                    )
+            );
+
+            if (!HookManager.isAuthUsed()) {
+                if (DailyRewardsPlugin.getRewardManager().processAutoClaimForUser(user)) {
+                    return;
+                }
+            }
+
+            if (!PermissionUtil.hasPermission(player, PermissionUtil.Permission.JOIN_NOTIFICATION_SETTING)) {
+                return;
+            }
+
+            if (!user.hasSettingEnabled(Setting.JOIN_NOTIFICATION)) {
+                return;
+            }
+
+            final Set<RewardType> availableRewards = user.getAvailableRewards();
+            if (availableRewards.isEmpty()) {
+                return;
+            }
+
+            ReminderReceiveEvent reminderReceiveEvent = new ReminderReceiveEvent(player, availableRewards);
+            Bukkit.getPluginManager().callEvent(reminderReceiveEvent);
+
+            if (reminderReceiveEvent.isCancelled()) {
+                return;
+            }
+
+            joinNotificationTask.addUser(user);
+        });
+    }
+
+    @EventHandler
+    private void onQuit(PlayerQuitEvent event) {
+        User user = UserHandler.removeUser(event.getPlayer().getUniqueId());
+        joinNotificationTask.getPlayerRewardCheckTimes().remove(user);
     }
 }

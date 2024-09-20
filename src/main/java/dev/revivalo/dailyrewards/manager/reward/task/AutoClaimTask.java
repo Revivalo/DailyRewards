61007ce42763245528559fadfb8653baa02019ab
Adam Bíba <revivalo@athelion.eu>
2024-09-20 19:37:17 +0200

    Task for auto-claiming

---
+++ src/main/java/dev/revivalo/dailyrewards/manager/reward/task/AutoClaimTask.java
@@ -0,0 +1,45 @@
+package dev.revivalo.dailyrewards.manager.reward.task;
+
+import dev.revivalo.dailyrewards.DailyRewardsPlugin;
+import dev.revivalo.dailyrewards.configuration.file.Config;
+import dev.revivalo.dailyrewards.user.User;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AutoClaimTask implements Task{
+    private final Map<User, Long> playerRewardCheckTimes = new ConcurrentHashMap<>();
+    private final Map<UUID, User> usersHashMap;
+
+    public AutoClaimTask(Map<UUID, User> usersHashMap) {
+        this.usersHashMap = usersHashMap;
+    }
+
+    public void addUser(User user) {
+        long checkTime = System.currentTimeMillis() + (Config.JOIN_NOTIFICATION_DELAY.asInt() * 1000L);
+
+        playerRewardCheckTimes.put(user, checkTime);
+    }
+
+    @Override
+    public BukkitRunnable get() {
+        return new BukkitRunnable() {
+            @Override
+            public void run() {
+                long currentTime = System.currentTimeMillis();
+
+                for (User user : usersHashMap.values()) {
+                    if (!playerRewardCheckTimes.containsKey(user)) continue;
+
+                    long checkTime = playerRewardCheckTimes.get(user);
+                    if (currentTime >= checkTime) {
+                        DailyRewardsPlugin.get().runSync(() -> DailyRewardsPlugin.getRewardManager().processAutoClaimForUser(user));
+                        playerRewardCheckTimes.remove(user);
+                    }
+                }
+            }
+        };
+    }
+}

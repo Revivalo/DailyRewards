0c169a771063fabb9ddb8bd13cef4b7158b9752b
Adam Bíba <revivalo@athelion.eu>
2024-09-20 15:18:19 +0200

    Shrinking

--- src/main/java/dev/revivalo/dailyrewards/manager/MenuManager.java
+++ src/main/java/dev/revivalo/dailyrewards/manager/MenuManager.java
@@ -4,6 +4,8 @@ import dev.revivalo.dailyrewards.DailyRewardsPlugin;
 import dev.revivalo.dailyrewards.configuration.file.Config;
 import dev.revivalo.dailyrewards.configuration.file.Lang;
 import dev.revivalo.dailyrewards.manager.reward.Reward;
+import dev.revivalo.dailyrewards.manager.reward.RewardType;
+import dev.revivalo.dailyrewards.manager.reward.action.ClaimAction;
 import dev.revivalo.dailyrewards.user.User;
 import dev.revivalo.dailyrewards.user.UserHandler;
 import dev.revivalo.dailyrewards.util.ItemBuilder;
@@ -12,6 +14,9 @@ import dev.revivalo.dailyrewards.util.TextUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
@@ -21,13 +26,14 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class MenuManager {
+public class MenuManager implements Listener {
     private ItemStack backgroundItem;
 
     private final InventoryHolder MAIN_MENU_HOLDER = new RewardsInventoryHolder();
     private final InventoryHolder SETTINGS_MENU_HOLDER = new RewardSettingsInventoryHolder();
 
     public MenuManager() {
+        DailyRewardsPlugin.get().registerListeners(this);
         loadBackgroundFiller();
     }
 
@@ -154,6 +160,63 @@ public class MenuManager {
         );
 
         player.openInventory(settings);
+    }
+
+    @EventHandler
+    public void onInventoryClick(final InventoryClickEvent event){
+        if (!(event.getInventory().getHolder() instanceof MenuManager.RewardsInventoryHolder)) return;
+        if (event.getCurrentItem() == null) return;
+        event.setCancelled(true);
+        final Player player = (Player) event.getWhoClicked();
+        int slot = event.getSlot();
+        if (Config.DAILY_POSITIONS.asIntegerList().contains(slot)){
+            new ClaimAction(player).preCheck(player, RewardType.DAILY);
+        } else if (Config.WEEKLY_POSITIONS.asIntegerList().contains(slot)) {
+            new ClaimAction(player).preCheck(player, RewardType.WEEKLY);
+        } else if (Config.MONTHLY_POSITIONS.asIntegerList().contains(slot)) {
+            new ClaimAction(player).preCheck(player, RewardType.MONTHLY);
+        } else if (slot == Config.SETTINGS_POSITION.asInt()) {
+            DailyRewardsPlugin.getMenuManager().openSettings(player);
+        }
+    }
+
+    @EventHandler
+    public void inventoryClick(final InventoryClickEvent event) {
+        if (!(event.getInventory().getHolder() instanceof MenuManager.RewardSettingsInventoryHolder)) {
+            return;
+        }
+
+        if (event.getCurrentItem() == null) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        final User user = UserHandler.getUser(event.getWhoClicked().getUniqueId());
+        final Player player = user.getPlayer();
+
+        int slot = event.getSlot();
+        if (slot == Config.JOIN_NOTIFICATION_POSITION.asInt()) {
+            if (!PermissionUtil.hasPermission(player, PermissionUtil.Permission.JOIN_NOTIFICATION_SETTING)) {
+                player.sendMessage(Lang.PERMISSION_MSG.asColoredString(player));
+                return;
+            }
+
+            user.toggleSetting(Setting.JOIN_NOTIFICATION, !user.hasSettingEnabled(Setting.JOIN_NOTIFICATION));
+
+            DailyRewardsPlugin.getMenuManager().openSettings(user.getPlayer());
+        } else if (slot == Config.AUTO_CLAIM_REWARDS_POSITION.asInt()) {
+            if (!PermissionUtil.hasPermission(player, PermissionUtil.Permission.AUTO_CLAIM_SETTING)) {
+                player.sendMessage(Lang.PERMISSION_MSG.asColoredString(player));
+                return;
+            }
+
+            user.toggleSetting(Setting.AUTO_CLAIM, !user.hasSettingEnabled(Setting.AUTO_CLAIM));
+
+            DailyRewardsPlugin.getMenuManager().openSettings(user.getPlayer());
+        } else if (slot == Config.SETTINGS_POSITION.asInt()) {
+            DailyRewardsPlugin.getMenuManager().openRewardsMenu(user.getPlayer());
+        }
     }
 
     public void loadBackgroundFiller() {

45798b15124a53aa7e4d8cbf5e2365b0d8af6229
Adam Bíba <revivalo@athelion.eu>
2024-12-04 23:52:20 +0100

    No need since new color util uses reflection

--- src/main/java/dev/revivalo/dailyrewards/util/VersionUtil.java
+++ src/main/java/dev/revivalo/dailyrewards/util/VersionUtil.java
@@ -5,7 +5,6 @@ import io.github.g00fy2.versioncompare.Version;
 import org.bukkit.Bukkit;
 
 public class VersionUtil {
-    private static boolean hexSupport;
     private static boolean oldVersion;
     private static boolean legacyVersion;
     public static boolean latestVersion;
@@ -13,21 +12,12 @@ public class VersionUtil {
     static {
         final String serverVersion = Bukkit.getBukkitVersion();
         Version version = new Version(serverVersion);
-        setHexSupport(version.isAtLeast("1.16"));
         setOldVersion(version.isLowerThan("1.9"));
         setLegacyVersion(version.isLowerThan("1.13"));
     }
 
     public static boolean isLoaded(String pluginName){
         return DailyRewardsPlugin.get().getPluginManager().getPlugin(pluginName) != null;
-    }
-
-    public static boolean isHexSupport() {
-        return hexSupport;
-    }
-
-    public static void setHexSupport(boolean hexSupport) {
-        VersionUtil.hexSupport = hexSupport;
     }
 
     public static boolean isOldVersion() {
